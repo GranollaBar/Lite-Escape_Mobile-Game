deb492eb9209284347ef4d8ef7f433631fa3c0fb
GranollaBar <josnoble113@gmail.com>
2020-12-06 20:09:54 +0000

    newer

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class KillPlayer : MonoBehaviour
+{
+    public int respawn;
+    private GameObject player;
+    private GameObject cam;
+    public GameObject death;
+
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            Destroy(player);
+            stopCam();
+            StartCoroutine(waiter());
+            death.SetActive(true);
+        }
+        
+    }
+
+    IEnumerator waiter()
+    {
+        //Wait for 4 seconds
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(respawn);
+    }
+
+    void stopCam()
+    {
+        cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        var cinemachine = cam.GetComponent<Cinemachine.CinemachineBrain>();
+        cinemachine.enabled = false;
+    }
+}
