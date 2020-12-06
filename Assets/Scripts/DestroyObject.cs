853ad835b2d191a172f3ecc5fefc6efddce44751
GranollaBar <josnoble113@gmail.com>
2020-12-06 17:04:59 +0000

    newer

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public GameObject deatheffect;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            deatheffect.SetActive(true);
+        }
+    }
+}
