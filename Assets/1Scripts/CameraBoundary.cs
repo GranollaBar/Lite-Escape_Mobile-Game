ef467b2c860cb16b318a0b5b40df4d05e78fef55
GranollaBar <josnoble113@gmail.com>
2020-12-12 17:31:17 +0000

    mobile game #2

---
+++ Assets/1Scripts/CameraBoundary.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraBoundary : MonoBehaviour
+{
+    [SerializeField]
+    float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float topLimit;
+    [SerializeField]
+    float bottomLimit;
+
+    void Update()
+    {
+        transform.position = new Vector3
+        (
+          Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+          Mathf.Clamp(transform.position.x, bottomLimit, topLimit),
+          transform.position.z
+        );  
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
+    }
+}
