6481db32a651a2509d03ee02be2d22af5a4669ba
GranollaBar <josnoble113@gmail.com>
2020-12-13 12:46:12 +0000

    mobile game #3

---
+++ Assets/1Scripts/LevelLoader2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader2 : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel2();
+        }
+    }
+
+    public void LoadNextLevel2()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Circle Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
