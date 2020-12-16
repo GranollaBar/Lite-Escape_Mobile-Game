5bb0f9ebebb592f4982c3ea32072d638a9ac1d90
GranollaBar <josnoble113@gmail.com>
2020-12-16 17:04:03 +0000

    Mobile Game #5

--- Assets/1Scripts/AutoMovePlayer.cs
+++ Assets/1Scripts/AutoMovePlayer.cs
@@ -1,37 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AutoMovePlayer : MonoBehaviour
 {
-    public PlayerController playerMovement;
-
-    private GameObject autoMove;
+    private GameObject exitDoor;
     private GameObject player;
+    private Rigidbody2D playerRb;
+    private Transform playerPosition;
+    private bool movePlayer = false; 
+    private int moveTo;
 
     // Start is called before the first frame update
     void Start()
     {
-        autoMove = GameObject.FindGameObjectWithTag("AutoMovePlayer");
+        exitDoor = GameObject.FindGameObjectWithTag("ExitDoor");
         player = GameObject.FindGameObjectWithTag("Player");
-        playerMovement = player.GetComponent<PlayerController>();
+        playerRb = player.GetComponent<Rigidbody2D>();
+        playerPosition = player.GetComponent<Transform>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject == autoMove)
+        if (other.gameObject == exitDoor)
+        {
+            Destroy(player.GetComponent<ballcontrol>());
+            playerRb.bodyType = RigidbodyType2D.Kinematic;
+            playerRb.constraints = RigidbodyConstraints2D.FreezePositionY; 
+            moveTo = 20;
+            movePlayer = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (movePlayer)
         {
             PlayerAutoMove();
-        }
+            if (Mathf.Abs(playerPosition.position.x - moveTo) < 2)
+            {
+                movePlayer = false;
+                playerRb.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
 
+        }
     }
 
     void PlayerAutoMove()
     {
-        ballcontrol ball = player.GetComponent<ballcontrol>();
-        ball.enabled = false;
-
-        playerMovement.Move(100 * Time.fixedDeltaTime * 1, false);
+        int moveAmount = 10;
+        Vector3 targetVelocity = new Vector2(moveAmount * 1f, playerRb.velocity.y);
+        Vector3 m_Velocity = Vector3.zero;
+        float m_MovementSmoothing = .05f;
+        playerRb.velocity = Vector3.SmoothDamp(playerRb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
     }
 
 }
